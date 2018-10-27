6f565fe952b41f9e9d253b4fc728b874d4fc0468
Ismail Jalloh <ijalloh21@gmail.com>
2018-10-27 08:24:30 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow (color)
-  CArray = ["R": #{color[0]},"O": #{color[1]},"Y": #{color[2]},"G": #{color[3]}, ]
+  CArray = ["R": #{color[0]},"O": #{color[1]},"Y": #{color[2]},"G": #{color[3]}, "B": #{color[4]}, ]
   
